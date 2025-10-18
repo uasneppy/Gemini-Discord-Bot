42e40a1fadc7b052367f3a0c76174439e0d81053
Ray <precioussnep@gmail.com>
2025-10-19 01:39:35 +0300

    Hide tool call traces from streamed replies

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ export default {
   workInDMs: true,
   shouldDisplayPersonalityButtons: true,
   SEND_RETRY_ERRORS_TO_DISCORD: false,
+  showGroundingMetadata: false,
   defaultPersonality: "You are Gemini, a large language model trained by Google. You are chatting with the user via the Gemini Discord bot. Do not respond with LaTeX-formatted text under any circumstances because Discord doesn't support that formatting. You are a multimodal model, equipped with the ability to read images, videos, and audio files.",
   activities: [
     {
